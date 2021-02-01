a509580f27709dce76111dd145a48f984f578979
kiruthika-kittusamy <kiruthika@5gindia.net>
2021-02-01 11:37:00 +0530

    first file

---
+++ addition.py
@@ -0,0 +1,26 @@
+#addition program
+a=10
+b=10
+c=a+b
+print("addition of two numbers",c)
+
+#getting inputs from the user in run time
+
+first_number=input("enter the first number:")
+second_number=input("enter the second number:")
+addition_results=first_number+second_number
+print("addition of given two numbers:",addition_results)
+
+#getting input from the user in particular data type
+
+weight=float(input("enter your weight"))
+print("entered weight:",weight)
+
+
+#display the string using {}
+employee_name="abc"  
+print("employee name:",employee_name)
+employee_designation="trainee"
+print("employee_name:{} and employee_designation:{}".format(employee_name,employee_designation))
+
+
